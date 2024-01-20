a38bb4c5d8c367d9ca0ea75800b6bc104642862d
ayush patel <ayushpatel1248@gmail.com>
2024-01-20 11:39:25 +0530

    added mongo db url in db.js

--- db/db.js
+++ db/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-// ******************************add your own database url below***********************************************
+// ******************************add database url below***********************************************
 
-MONGODB_URI = ''                   
+MONGODB_URI = 'mongodb+srv://ayushpatel062004:Gh7ypCDLEPJnuuib@proprogrammers.r6bjbcn.mongodb.net/ECommerce?retryWrites=true&w=majority'                   
 
 mongoose.connect(MONGODB_URI).then(res => {
     console.log("connected to database server successfully")
