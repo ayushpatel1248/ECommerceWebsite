8e767c6e236e6c74cee8e39fc6e03545eb09b7dc
ayush patel <ayushpatel1248@gmail.com>
2024-01-26 12:23:48 +0530

    readme

--- routes/CartRoutes.js
+++ routes/CartRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const CartController = require("../controllers/CartController")
 
 router.post('/addtocart', CartController.addToCart)
-router.post('/viewCart', CartController.viewCart)
+router.post('/viewCart', CartController.viewCart)   
 router.post('/removeProduct', CartController.removeProduct)
 router.post('/updateCart', CartController.updateCart)
 module.exports = router
