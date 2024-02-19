5d1fab0720bdd0e5d8e9f6eb332419729941094a
ayush patel <ayushpatel1248@gmail.com>
2024-02-20 00:25:12 +0530

    all products page with filter ui and api integration complete

--- client/src/store/slices/getProductDataSlice.js
+++ client/src/store/slices/getProductDataSlice.js
@@ -22,5 +22,16 @@ export const fetchProductData = (skip, limit) => async (dispatch) => {
     }
 };
 
+export const fetchFilteredProductData = (lowPrize, highPrize) => async (dispatch) => {
+    try {
+        const BASE_URL = process.env.REACT_APP_BASE_URL;
+        const response = await axios.get(`${BASE_URL}/filter`, { params: { "lowPrize": lowPrize, "highPrize": highPrize } });
+        const data = response.data.data;
+        dispatch(getProductData(data));
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+};
+
 export const { getProductData } = getProductDataSlice.actions
 export default getProductDataSlice.reducer;
