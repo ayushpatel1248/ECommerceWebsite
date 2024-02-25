d44fe8f9b3469fb5acc648a4d5ef729d7d47ad9a
ayush patel <ayushpatel1248@gmail.com>
2024-02-25 22:41:24 +0530

    add to cart done

--- client/src/Components/allProductsPage/AllProductsPage.jsx
+++ client/src/Components/allProductsPage/AllProductsPage.jsx
@@ -17,6 +17,12 @@ import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+import axios from 'axios';
+
+
+const baseUrl = process.env.REACT_APP_BASE_URL
 
 const AllProductsPage = () => {
 
@@ -25,6 +31,7 @@ const AllProductsPage = () => {
     // console.log("this is data from redux after fetching = ", getProductData)
     const [value, setValue] = React.useState([0, 400]);
     const [sideBarActive, setSideBarActive] = useState(false)
+    const notify = (notifyMessage) => toast(notifyMessage);
 
     useEffect(() => {
         dispatch(fetchProductData(1, 10))
@@ -38,6 +45,24 @@ const AllProductsPage = () => {
         setSideBarActive(false)
         dispatch(fetchFilteredProductData(value[0], value[1]))
     }
+
+    const handleAddToCart = async (el) => {
+        try {
+          const authorization = localStorage.getItem("authorization")
+          if (authorization) {
+            console.log(el)
+            const itemAddedToCart = await axios.post(`${baseUrl}/cart/addtocart`, { "productid": el._id }, { headers: { authorization: authorization } })
+            console.log(itemAddedToCart)
+            notify(itemAddedToCart.data.msg)
+          }
+          else {
+            notify("user must be login first")
+          }
+        }
+        catch (err) {
+          notify(err)
+        }
+      }
 
 
     // for range of priz code start
@@ -133,7 +158,7 @@ const AllProductsPage = () => {
                                         <div className="card-footer-product">
                                             <span className="text-title-product">{el.price}</span>
                                             <div class="card-button-product">
-                                                <svg class="svg-icon-product" viewBox="0 0 20 20" >
+                                                <svg class="svg-icon-product" viewBox="0 0 20 20"  onClick={() => handleAddToCart(el)}>
                                                     <path d="M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z"></path>
                                                     <path d="M8.251,12.386c-1.023,0-1.856,0.834-1.856,1.856s0.833,1.853,1.856,1.853c1.021,0,1.853-0.83,1.853-1.853S9.273,12.386,8.251,12.386z M8.251,15.116c-0.484,0-0.877-0.393-0.877-0.874c0-0.484,0.394-0.878,0.877-0.878c0.482,0,0.875,0.394,0.875,0.878C9.126,14.724,8.733,15.116,8.251,15.116z"></path>
                                                     <path d="M13.972,12.386c-1.022,0-1.855,0.834-1.855,1.856s0.833,1.853,1.855,1.853s1.854-0.83,1.854-1.853S14.994,12.386,13.972,12.386z M13.972,15.116c-0.484,0-0.878-0.393-0.878-0.874c0-0.484,0.394-0.878,0.878-0.878c0.482,0,0.875,0.394,0.875,0.878C14.847,14.724,14.454,15.116,13.972,15.116z"></path>
@@ -155,7 +180,7 @@ const AllProductsPage = () => {
                 <Pagination count={Math.ceil((getProductData.count.count / 10))} variant="outlined" color="primary" onChange={(e, value) => { handlePagination(value) }} />
             </div>
             <Footer />
-            {console.log(value)}
+            <ToastContainer></ToastContainer>
         </div >
     )
 }
--- client/src/Components/product/Products.jsx
+++ client/src/Components/product/Products.jsx
@@ -16,7 +16,8 @@ import { fetchProductData } from '../../store/slices/getProductDataSlice';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import Loader from '../Loader';
-
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
 
 
 const baseUrl = process.env.REACT_APP_BASE_URL
@@ -26,27 +27,39 @@ const baseUrl = process.env.REACT_APP_BASE_URL
 const Products = () => {
   const dispatch = useDispatch();
   const [index, setIndex] = useState(0);
-
+  const [isLoading , setIsLoading] = useState(false);
   const { width, height } = useWindowSize()
   const [confetti, setConfetti] = useState(true)
 
   const { getProductData } = useSelector(state => state);
   const navigate = useNavigate()
+  const notify = (notifyMessage) => toast(notifyMessage);
+
 
   const handleSelect = (selectedIndex) => {
     setIndex(selectedIndex);
   };
-  // this to be done properly 
-  const handleAddToCart = (el) => {
-    const authorization = localStorage.getItem("authorization")
-    if (authorization) {
-      console.log(el)
 
+
+  const handleAddToCart = async (el) => {
+    try {
+      setIsLoading(true)
+      const authorization = localStorage.getItem("authorization")
+      if (authorization) {
+        console.log(el)
+        const itemAddedToCart = await axios.post(`${baseUrl}/cart/addtocart`, { "productid": el._id }, { headers: { authorization: authorization } })
+        console.log(itemAddedToCart)
+        notify(itemAddedToCart.data.msg)
+      }
+      else {
+        notify("user must be logined ")
+      }
     }
-    else {
-      // axios.post(`${baseUrl}/cart/addtocart`)
-      console.log(authorization)
-
+    catch (err) {
+      notify(err)
+    }
+    finally{
+      setIsLoading(false)
     }
   }
 
@@ -96,7 +109,7 @@ const Products = () => {
             <div class="card-footer-product">
               <span class="text-title-product">{el.price}</span>
               <div class="card-button-product">
-                <svg class="svg-icon-product" viewBox="0 0 20 20" onClick={() => handleAddToCart(el)}>
+                <svg class="svg-icon-product" viewBox="0 0 20 20"  onClick={() => handleAddToCart(el)}>
                   <path d="M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z"></path>
                   <path d="M8.251,12.386c-1.023,0-1.856,0.834-1.856,1.856s0.833,1.853,1.856,1.853c1.021,0,1.853-0.83,1.853-1.853S9.273,12.386,8.251,12.386z M8.251,15.116c-0.484,0-0.877-0.393-0.877-0.874c0-0.484,0.394-0.878,0.877-0.878c0.482,0,0.875,0.394,0.875,0.878C9.126,14.724,8.733,15.116,8.251,15.116z"></path>
                   <path d="M13.972,12.386c-1.022,0-1.855,0.834-1.855,1.856s0.833,1.853,1.855,1.853s1.854-0.83,1.854-1.853S14.994,12.386,13.972,12.386z M13.972,15.116c-0.484,0-0.878-0.393-0.878-0.874c0-0.484,0.394-0.878,0.878-0.878c0.482,0,0.875,0.394,0.875,0.878C14.847,14.724,14.454,15.116,13.972,15.116z"></path>
@@ -104,7 +117,7 @@ const Products = () => {
               </div>
             </div>
           </div>
-        }): <Loader/>}
+        }) : <Loader />}
       </div>
 
       {/* all product button start  */}
@@ -306,6 +319,7 @@ const Products = () => {
 
 
       <Footer />
+      <ToastContainer />
     </div>
   )
 }
