5ae56f0d80c344630c63c21fc669eddfc82754f4
ayush patel <ayushpatel1248@gmail.com>
2024-03-02 14:12:29 +0530

    cart count

--- client/src/Components/productDescription/ProductDescription.jsx
+++ client/src/Components/productDescription/ProductDescription.jsx
@@ -7,13 +7,6 @@ import Footer from '../footer/Footer';
 import { useParams } from "react-router-dom";
 import { fetchProductDetail } from '../../store/slices/productDescSlice';
 import Loader from '../Loader';
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer, toast } from 'react-toastify';
-import axios from 'axios';
-
-
-const baseUrl = process.env.REACT_APP_BASE_URL
-
 
 const ProductDescription = () => {
     let { product_id } = useParams();
@@ -22,8 +15,6 @@ const ProductDescription = () => {
     const [isLoading, setIsLoading] = useState(true)
     const [isDiscountAvailable, setIsDiscountAvailable] = useState(false)
     const productData = useSelector((state) => state.productDesc.productDetail[0])
-    const notify = (notifyMessage) => toast(notifyMessage);
-
 
     useEffect(() => {
         setTimeout(() => {
@@ -32,6 +23,13 @@ const ProductDescription = () => {
         dispatch(fetchProductDetail(product_id))
         setIsLoading(false)
     }, [])
+    useEffect(() => {
+        if (productData?.discount) {
+            setIsDiscountAvailable(true)
+        } else {
+            setIsDiscountAvailable(false)
+        }
+    }, [productData])
     return (
         <div>
             {console.log(productData)}
@@ -99,32 +97,43 @@ const ProductDescription = () => {
 
                             </div>
                         </div>
-                        <div className='desc-buy-sell-div  '>
-                            {/* buy */}
-                            <div data-tooltip={`Price:₹${productData?.price}`} className="button-buy-desc buy-sell-desc">
-                                <div className="button-wrapper-buy-desc">
-                                    <div className="text-buy-desc">Buy Now</div>
-                                    <span className="icon-buy-desc">
-                                        <svg viewBox="0 0 16 16" className="bi bi-cart2" fill="currentColor" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"></path>
-                                        </svg>
+                        {/* buy sell */}     {/* price */}
+                        <div className='d-flex buy-sell-price-parent'>
+                            <div className='desc-buy-sell-div  '>
+                                {/* buy */}
+                                <div data-tooltip={`Price:₹${productData?.price}`} className="button-buy-desc buy-sell-desc">
+                                    <div className="button-wrapper-buy-desc">
+                                        <div className="text-buy-desc">Buy Now</div>
+                                        <span className="icon-buy-desc">
+                                            <svg viewBox="0 0 16 16" className="bi bi-cart2" fill="currentColor" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"></path>
+                                            </svg>
+                                        </span>
+                                    </div>
+                                </div>
+                                {/* buy-end */}
+                                {/* add to cart start */}
+                                <button className="CartBtn-add-to-cart-desc buy-sell-desc ">
+                                    <span className="IconContainer-add-to-cart-desc">
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512" fill="rgb(17, 17, 17)" class="cart"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg>
                                     </span>
+                                    <p className="text-add-to-cart-desc pt-3">Add to Cart</p>
+                                </button>
+                                {/* add to cart end */}
+                            </div>
+                            <div className='pl-4 price-desc '>
+                                <div className='border-top description-text description-text-div pt-4 price-div' style={{ opacity: opacity }}>
+                                    <h2 className='description-text font-weight-600 description-text-div font-family' style={{ opacity: opacity }}>Price :</h2>
+                                    <h1 className={isDiscountAvailable ? 'font-family text-decoration-desc' : "font-family"}>{productData?.price} ₹</h1>
+                                </div>
+                                <div className={isDiscountAvailable ? "font-family description-text description-text-div " : "display-none-desc  font-family"} style={{ opacity: opacity }}>
+                                    <h3 className="description-text red-yellow-animation-desc" style={{ opacity: opacity }}>Now available in only : {(parseInt(productData?.price) - ((parseInt(productData?.price) / 100) * parseInt(productData?.discount)))}₹</h3>
                                 </div>
                             </div>
-                            {/* buy-end */}
-                            {/* add to cart start */}
-                            <button className="CartBtn-add-to-cart-desc buy-sell-desc ">
-                                <span className="IconContainer-add-to-cart-desc">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512" fill="rgb(17, 17, 17)" class="cart"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg>
-                                </span>
-                                <p className="text-add-to-cart-desc pt-3">Add to Cart</p>
-                            </button>
-                            {/* add to cart end */}
                         </div>
                     </div>
 
                 </div>}
-                <ToastContainer/>
         </div>
 
     )
